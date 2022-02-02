ab9c276cfa5433caaad24802fdf3e129a5f0fca5
somaye <nasirisomayeh2@gmail.com>
2022-02-02 22:11:27 +0330

    Graphic_shape

---
+++ Graphic_shape.py
@@ -0,0 +1,14 @@
+# شکل گرافیکی
+
+import os
+os.system ('cls')
+
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range (200):
+    t.pencolor(colors[x%6])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
